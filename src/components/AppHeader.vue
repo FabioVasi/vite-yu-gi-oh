9f45c5771ee5843d478f6cbadc7a4055991f3482
FabioVasi <vasifabio96@gmail.com>
2023-09-26 14:09:39 +0200

    Initial commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'AppHeader'
+    }
+  }
+}
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss">
+@use '../assets/scss/partials/variables.scss' as *;
+
+</style>
